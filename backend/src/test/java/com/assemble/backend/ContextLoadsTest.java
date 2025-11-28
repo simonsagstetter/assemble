2afcde7739c0799e6be442e65d4ff9b392e5da5a
Simon Sagstetter <simon.sagstetter95@gmail.com>
2025-11-28 08:58:56 +0100

    Setup Environment, Shell, Docker, Databases, Testcontainers, CI and Test

--- backend/src/test/java/com/assemble/backend/ContextLoadsTest.java
+++ backend/src/test/java/com/assemble/backend/ContextLoadsTest.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class BackendApplicationTests {
+class ContextLoadsTest {
 
     @Test
     void contextLoads() {
--- backend/src/test/java/com/assemble/backend/TestcontainersConfiguration.java
+++ backend/src/test/java/com/assemble/backend/TestcontainersConfiguration.java
@@ -3,8 +3,8 @@ package com.assemble.backend;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.mongodb.MongoDBContainer;
-import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -18,8 +18,8 @@ class TestcontainersConfiguration {
 
     @Bean
     @ServiceConnection
-    PostgreSQLContainer postgresContainer() {
-        return new PostgreSQLContainer( DockerImageName.parse( "postgres:latest" ) );
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>( DockerImageName.parse( "postgres:latest" ) );
     }
 
 }
