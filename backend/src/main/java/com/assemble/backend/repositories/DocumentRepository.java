f6ca9f08448079173a98f9cbbc907ef192101304
Simon Sagstetter <simon.sagstetter95@gmail.com>
2025-12-04 13:24:29 +0100

    Added repository keyword

--- backend/src/main/java/com/assemble/backend/repositories/DocumentRepository.java
+++ backend/src/main/java/com/assemble/backend/repositories/DocumentRepository.java
@@ -2,6 +2,8 @@ package com.assemble.backend.repositories;
 
 import com.assemble.backend.models.db.DocumentGreeting;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DocumentRepository extends MongoRepository<DocumentGreeting, String> {
 }
