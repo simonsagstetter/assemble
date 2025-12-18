470bfa4fc23c573b02bd4b69308496982b90fc9d
Simon Sagstetter <simon.sagstetter95@gmail.com>
2025-12-18 16:26:25 +0100

    Added user delete route

---
+++ frontend/src/app/app/admin/@modal/(.)users/[id]/delete/loading.tsx
@@ -0,0 +1,19 @@
+/*
+ * assemble
+ * page.tsx
+ *
+ * Copyright (c) 2025 Simon Sagstetter
+ *
+ * This software is the property of Simon Sagstetter.
+ * All rights reserved.
+ */
+
+import Loading from "@/components/custom-ui/Loading";
+import ModalHeader from "@/components/custom-ui/ModalHeader";
+
+export default function LoadingPage() {
+    return <ModalHeader title={ "Delete" } description={ "Confirm the action below to delete this user." }
+                        entity={ "user" }>
+        <Loading title={ "Loading User Details" }/>
+    </ModalHeader>
+}
