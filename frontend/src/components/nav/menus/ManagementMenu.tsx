dea9d4f4e36f06ab058d5fbe17e6b6d4fd97f2e9
Simon Sagstetter <simon.sagstetter95@gmail.com>
2025-12-22 09:52:40 +0100

    Added ManagmentMenu

--- frontend/src/components/nav/menus/ManagementMenu.tsx
+++ frontend/src/components/nav/menus/ManagementMenu.tsx
@@ -1,6 +1,6 @@
 /*
  * assemble
- * AdministrationMenu.tsx
+ * ManagementMenu.tsx
  *
  * Copyright (c) 2025 Simon Sagstetter
  *
@@ -10,37 +10,42 @@
 
 "use client"
 
-import { ListIcon, MoreHorizontal, PlusIcon, UserIcon } from "lucide-react"
+import {
+    MoreHorizontal,
+    ListIcon, PlusIcon, IdCardIcon,
+} from "lucide-react"
 
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import {
     SidebarGroup,
     SidebarGroupLabel,
-    SidebarMenu, SidebarMenuAction,
+    SidebarMenu,
+    SidebarMenuAction,
     SidebarMenuButton,
     SidebarMenuItem,
     useSidebar,
 } from "@/components/ui/sidebar"
 import Link from "next/link";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu";
 import { useRouter } from "@bprogress/next/app";
 
-export function AdministrationMenu() {
+export default function ManagementMenu() {
     const router = useRouter();
-    const { isMobile } = useSidebar();
+    const { isMobile } = useSidebar()
+
     return (
         <SidebarGroup className="group-data-[collapsible=icon]:hidden">
-            <SidebarGroupLabel>Adminnistration</SidebarGroupLabel>
+            <SidebarGroupLabel>Management</SidebarGroupLabel>
             <SidebarMenu>
                 <SidebarMenuItem>
                     <SidebarMenuButton asChild>
-                        <Link href="/app/admin/users">
-                            <UserIcon></UserIcon>
-                            <span>Users</span>
+                        <Link href="/app/manage/employees">
+                            <IdCardIcon></IdCardIcon>
+                            <span>Employees</span>
                         </Link>
                     </SidebarMenuButton>
                     <DropdownMenu>
@@ -55,11 +60,11 @@ export function AdministrationMenu() {
                             side={ isMobile ? "bottom" : "right" }
                             align={ isMobile ? "end" : "start" }
                         >
-                            <DropdownMenuItem onSelect={ () => router.push( "/app/admin/users" ) }>
+                            <DropdownMenuItem onSelect={ () => router.push( "/app/manage/employees" ) }>
                                 <ListIcon className="text-muted-foreground"/>
                                 <span>View All</span>
                             </DropdownMenuItem>
-                            <DropdownMenuItem onSelect={ () => router.push( "/app/admin/users/create" ) }>
+                            <DropdownMenuItem onSelect={ () => router.push( "/app/manage/employees/create" ) }>
                                 <PlusIcon className="text-muted-foreground"/>
                                 <span>New</span>
                             </DropdownMenuItem>
@@ -68,6 +73,5 @@ export function AdministrationMenu() {
                 </SidebarMenuItem>
             </SidebarMenu>
         </SidebarGroup>
-
     )
 }
