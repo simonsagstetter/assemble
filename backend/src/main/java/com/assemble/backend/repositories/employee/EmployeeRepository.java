a48ba5f228732574f08e42fa4edb2103a6d612bd
Simon Sagstetter <simon.sagstetter95@gmail.com>
2025-12-29 13:55:40 +0100

    Added findByIdIsIn repo methods

--- backend/src/main/java/com/assemble/backend/repositories/employee/EmployeeRepository.java
+++ backend/src/main/java/com/assemble/backend/repositories/employee/EmployeeRepository.java
@@ -16,6 +16,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -31,5 +32,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
                 )
             """)
     List<Employee> search( @Param("searchTerm") String searchTerm );
+
+    List<Employee> findAllByIdIsIn( Collection<UUID> ids );
 
 }
--- backend/src/main/java/com/assemble/backend/repositories/project/ProjectRepository.java
+++ backend/src/main/java/com/assemble/backend/repositories/project/ProjectRepository.java
@@ -14,7 +14,11 @@ package com.assemble.backend.repositories.project;
 import com.assemble.backend.models.entities.project.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 public interface ProjectRepository extends JpaRepository<Project, UUID> {
+
+    List<Project> findAllByIdIsIn( Collection<UUID> ids );
 }
