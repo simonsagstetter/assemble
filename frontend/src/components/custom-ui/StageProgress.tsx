8bda0bd8f56a9c643aeb1b2d2f40753537ed7e24
Simon Sagstetter <simon.sagstetter95@gmail.com>
2025-12-30 10:23:08 +0100

    Updated unsafe type assertion

--- frontend/src/components/custom-ui/StageProgress.tsx
+++ frontend/src/components/custom-ui/StageProgress.tsx
@@ -24,7 +24,7 @@ export default function StageProgress<T>( { label, values, value }: StageProgres
 
     return <div className={ "flex flex-col pb-1 px-8" }>
         <small className={ "text-xs uppercase leading-2" }>{ label }</small>
-        <h2 className={ "font-bold text-2xl pb-1 tracking-wide" }>{ value as string }</h2>
+        <h2 className={ "font-bold text-2xl pb-1 tracking-wide" }>{ String( value ) }</h2>
         <Progress value={ getStageProgressPercent( value ) } className={ "h-3 [&_div]:bg-blue-800" }/>
     </div>
 }
