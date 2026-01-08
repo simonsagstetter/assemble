98cdcda9140a61fd85d8960de2d0bdd4f86b9531
Simon Sagstetter <simon.sagstetter95@gmail.com>
2026-01-08 16:12:08 +0100

    Added datetime and duration utils

---
+++ frontend/src/utils/duration.ts
@@ -0,0 +1,53 @@
+/*
+ * assemble
+ * duration.ts
+ *
+ * Copyright (c) 2026 Simon Sagstetter
+ *
+ * This software is the property of Simon Sagstetter.
+ * All rights reserved.
+ */
+import moment from "moment";
+
+function parseIsoDuration( iso: string ) {
+    const duration = moment.duration( iso );
+    return { hours: duration.hours(), minutes: duration.minutes(), seconds: duration.seconds() };
+}
+
+function dateStringToMs( dateString: string ) {
+    const date = new Date( dateString );
+    return ( date.getHours() * 60 + date.getMinutes() ) * 60 * 1000;
+}
+
+function isoDurationToMs( iso: string ) {
+    const { hours, minutes } = parseIsoDuration( iso );
+    return ( hours * 60 + minutes ) * 60 * 1000;
+}
+
+function msToHHmm( ms: number ): string {
+    const totalMinutes = Math.floor( ms / ( 60 * 1000 ) );
+    const h = Math.floor( totalMinutes / 60 );
+    const m = totalMinutes % 60;
+
+    return `${ String( h ).padStart( 2, "0" ) }:${ String( m ).padStart( 2, "0" ) }`;
+}
+
+function HHmmToMs( time: string ) {
+    const [ h, m ] = time.split( ":" ).map( Number );
+    return h * 60 * 60 * 1000 + m * 60 * 1000;
+}
+
+function toDuration( time?: string ) {
+    if ( !time ) return undefined;
+    const [ h, m ] = time.split( ":" ).map( Number );
+
+    return `PT${ h }H${ m }M`;
+}
+
+export {
+    toDuration,
+    dateStringToMs,
+    isoDurationToMs,
+    msToHHmm,
+    HHmmToMs
+}
