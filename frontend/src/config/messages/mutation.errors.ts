183af29ef16a02369a59a1eb2bba2c351bae9e82
Simon Sagstetter <simon.sagstetter95@gmail.com>
2026-04-16 10:42:42 +0200

    Added mutation.errors.ts to provide basic error message in future

---
+++ frontend/src/config/messages/mutation.errors.ts
@@ -0,0 +1,17 @@
+/*
+ * assemble
+ * mutation.errors.ts.tsx
+ *
+ * Copyright (c) 2026 Simon Sagstetter
+ *
+ * This software is the property of Simon Sagstetter.
+ * All rights reserved.
+ */
+
+const MUTATION_ERRORS = {
+    UNKOWN: "We ran into a problem. Please try again.",
+} as const;
+
+export {
+    MUTATION_ERRORS
+};
